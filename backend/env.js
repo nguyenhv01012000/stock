383ebf22f1c31738fe2726353bf4b44fb82fe618
Hoàng Nguyên <nguyenhd01012000@gmail.com>
2023-11-05 10:40:45 +0700

    fix bug

--- backend/env.js
+++ backend/env.js
@@ -1,1 +1,1 @@
-const BACKEND = "123.31.29.193:4004"
+const BACKEND = "http://123.31.29.193:4004"
--- frontend/src/env.js
+++ frontend/src/env.js
@@ -1,1 +1,1 @@
-export const BACKEND = "123.31.29.193:4004"
+export const BACKEND = "http://123.31.29.193:4004"
