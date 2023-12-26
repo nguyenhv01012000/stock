d8d2ce153a6b62047cee69da93e471256390df45
Hoàng Nguyên <nguyenhd01012000@gmail.com>
2023-12-26 21:39:32 +0700

    fix bug

--- frontend/src/env.js
+++ frontend/src/env.js
@@ -1,2 +1,3 @@
-export const BACKEND = "http://123.31.29.193:4004"
+// export const BACKEND = "http://123.31.29.193:4004"
 // export const BACKEND = "http://localhost:4000"
+export const BACKEND = "http://103.221.222.134:4004"
