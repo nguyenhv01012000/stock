3f7ebf07411da29e4e7f33b8ac76bf7045d45670
Hoàng Nguyên <nguyenhd01012000@gmail.com>
2024-01-28 16:05:29 +0700

    fix bug

--- frontend/src/env.js
+++ frontend/src/env.js
@@ -1,5 +1,5 @@
 // export const BACKEND = "http://123.31.29.193:4004"
-export const BACKEND = "http://localhost:4004"
+// export const BACKEND = "http://localhost:4004"
 // export const BACKEND = "http://103.221.222.134:4004"
-// export const BACKEND = "https://api.chungkhoandomino.com"
+export const BACKEND = "https://api.chungkhoandomino.com"
 
